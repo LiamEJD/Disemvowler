194b43574dd837423e570a7b13ff2c9a977d12dc
LiamEJD <12356299@students.lincoln.ac.uk>
2015-05-12 21:56:35 +0100

    First Commit

---
+++ ConsoleApplication7/Disemvowel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disemvowel
+{
+    public static class Disemvowel
+    {
+        public static string[] Replace(string s, char[] seperators)
+        {
+            string removed = "";
+            foreach (char c in s)
+            {
+                for (int i = 0; i < seperators.Count(); i++)
+                {
+                    if (c == seperators[i])
+                    {
+                        removed += c;
+                    }
+                }
+            }
+            string output = string.Concat(s.Split(seperators));
+            string[] result = {output, removed};
+            return result;
+        }
+    }
+}
---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Disemvowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter original sentence: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("Enter characters to be removed (including a space character if needed): ");
+            string delimeter = Console.ReadLine();
+            char[] seperator = delimeter.ToCharArray();
+            string[] results = Disemvowel.Replace(text, seperator);
+            Console.WriteLine("New sentence: {0}\nCharacters removed: {1}", results[0], results[1]);
+        }
+    }
+}
